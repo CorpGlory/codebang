c1e4bc58dfcfbc830238196ce1a4d999a7a0110c
Alexey Velikiy <av@corpglory.com>
2018-12-27 02:08:24 +0300

    basic bars

--- vis/src/chart.js
+++ vis/src/chart.js
@@ -1,27 +1,42 @@
 var echarts = require('echarts');
 
 
+var option = {
+  title: {
+    text: 'Numerical method two stabs collision',
+  },
+  color: ['#3398DB'],
+  animation: false,
+  grid: {
+      left: '1%',
+      right: '1%',
+      bottom: '40px',
+  },
+  xAxis: {
+      type: 'value',
+      boundaryGap: [0, 1],
+      min: -1,
+      max: 1,
+      interval: 0.1
+  },
+  yAxis: {
+      type:'category',
+      data: ['1d'],
+      show: false
+  },
+  series: [
+    { type: 'bar', stack: '1d', data: [0.1], color: 'red' },
+    { type: 'bar', stack: '1d', data: [0.1], color: 'blue' }
+  ]
+};
+
+
 export function setupChart(elem) {
   
   // initialize echarts instance with prepared DOM
   var myChart = echarts.init(elem);
 
   // draw chart
-  myChart.setOption({
-      title: {
-          text: 'ECharts entry example'
-      },
-      tooltip: {},
-      xAxis: {
-          data: ['shirt', 'cardign', 'chiffon shirt', 'pants', 'heels', 'socks']
-      },
-      yAxis: {},
-      series: [{
-          name: 'sales',
-
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-      }]
-  });
+  myChart.setOption(option);
 
 }
