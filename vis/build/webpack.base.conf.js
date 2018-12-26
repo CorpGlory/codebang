9449c287365e6b6f4a12b360dd886d386844a4b0
Alexey Velikiy <av@corpglory.com>
2018-12-26 22:31:22 +0300

    basic webpack vis project

---
+++ vis/build/webpack.base.conf.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+const path = require('path');
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, '..', 'dist')
+  },
+  module: {
+    rules: []
+  }
+};
---
+++ vis/src/index.js
@@ -0,0 +1,30 @@
+var $ = require('jquery');
+var echarts = require('echarts');
+
+$(document).ready(function() {
+  alert('loaded');
+})
+
+// // initialize echarts instance with prepared DOM
+// var myChart = echarts.init(document.getElementById('main'));
+
+// alert('hey bitch');
+// alert('i fuck you')
+
+// // draw chart
+// myChart.setOption({
+//     title: {
+//         text: 'ECharts entry example'
+//     },
+//     tooltip: {},
+//     xAxis: {
+//         data: ['shirt', 'cardign', 'chiffon shirt', 'pants', 'heels', 'socks']
+//     },
+//     yAxis: {},
+//     series: [{
+//         name: 'sales',
+
+//         type: 'bar',
+//         data: [5, 20, 36, 10, 10, 20]
+//     }]
+// });
