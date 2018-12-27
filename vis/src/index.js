257f34da51a6fe3783fb0a0d619059d04ce96e94
Alexey Velikiy <av@corpglory.com>
2018-12-27 04:01:41 +0300

    basic moving stabs

--- vis/src/index.js
+++ vis/src/index.js
@@ -1,6 +1,14 @@
 var $ = require('jquery');
-import { setupChart } from './chart'
+import { setupChart, tick } from './chart'
 
 $(document).ready(function() {
   setupChart(document.getElementById('main'));
+
+  $(document).keypress(function(k) {
+    if(k.key === ' ') {
+      tick();
+    }
+  });
+
 })
+
